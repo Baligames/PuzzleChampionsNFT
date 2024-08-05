7a9301de3e0ae85388a0aa8a9f30d7abb5e9b4d0
Carrera Seo <wonick.seo@baligames.xyz>
2024-08-05 20:22:30 +0900

    * ERC1155 초기파일.

---
+++ scripts/deploy/proxy/erc1155-upgradeable-proxy.ts
@@ -0,0 +1,25 @@
+import { PuzzleChampionsNFT__factory } from '../../typechain-types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import TransparentUpgradeableProxy from 'hardhat-deploy/extendedArtifacts/TransparentUpgradeableProxy.json';
+
+const erc721Interface = PuzzleChampionsNFT__factory.createInterface();
+
+const deploy = async ({ getNamedAccounts, deployments, network }: HardhatRuntimeEnvironment) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const logicContract = await deployments.get('PuzzleChampionsNFTLogic');
+
+  const data = erc721Interface.encodeFunctionData('initialize');
+
+  await deploy('PuzzleChampionsNFT', {  // PuzzleChampionsNFT Proxy
+    contract: TransparentUpgradeableProxy,
+    from: deployer,
+    log: true,
+    args: [logicContract.address, deployer ,data],
+  });
+};
+
+deploy.tags = ['PuzzleChampionsNFT'];
+deploy.dependencies = ['VerifyContracts', 'PuzzleChampionsNFTLogic'];
+
+export default deploy;
