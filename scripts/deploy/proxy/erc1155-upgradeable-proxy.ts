e84e11fa880afa566b1f605ca32ca4087d7d93cf
Carrera Seo <wonick.seo@baligames.xyz>
2024-08-06 11:38:01 +0900

    * erc1155-upgradeable-proxy.ts 오브젝트 명 변경

--- scripts/deploy/proxy/erc1155-upgradeable-proxy.ts
+++ scripts/deploy/proxy/erc1155-upgradeable-proxy.ts
@@ -2,14 +2,14 @@ import { PuzzleChampionsNFT__factory } from '../../typechain-types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import TransparentUpgradeableProxy from 'hardhat-deploy/extendedArtifacts/TransparentUpgradeableProxy.json';
 
-const erc721Interface = PuzzleChampionsNFT__factory.createInterface();
+const ercInterface = PuzzleChampionsNFT__factory.createInterface();
 
 const deploy = async ({ getNamedAccounts, deployments, network }: HardhatRuntimeEnvironment) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
   const logicContract = await deployments.get('PuzzleChampionsNFTLogic');
 
-  const data = erc721Interface.encodeFunctionData('initialize');
+  const data = ercInterface.encodeFunctionData('initialize');
 
   await deploy('PuzzleChampionsNFT', {  // PuzzleChampionsNFT Proxy
     contract: TransparentUpgradeableProxy,
